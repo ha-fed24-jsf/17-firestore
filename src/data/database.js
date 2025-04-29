12491f8837f34c830aec4635bc8fb07f36861269
David Andersson <lejonmanen@gmail.com>
2025-04-29 13:14:22 +0200

    Design done

---
+++ src/data/database.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDWUWZdYEUetjyf3PqJv017TzhMEptZlcc",
+  authDomain: "fed24-chat.firebaseapp.com",
+  projectId: "fed24-chat",
+  storageBucket: "fed24-chat.firebasestorage.app",
+  messagingSenderId: "781423246448",
+  appId: "1:781423246448:web:f09df2ab2e29c1355988b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
